335b4b1165e121487e53c83d50f0a246e31b4358
Gavin Palmer <gavinhughpalmer@gmail.com>
2023-01-16 15:22:37 +0000

    Updating Homebrew install link

--- osx_bootstrap.sh
+++ osx_bootstrap.sh
@@ -12,7 +12,7 @@ xcode-select --install
 # Check for Homebrew, install if we don't have it
 if test ! $(which brew); then
     echo "Installing homebrew..."
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Update homebrew recipes
